de8863b2ba02bceaae09247d9d6e8f9d4932a87c
txlsy <334224109@qq.com>
2015-09-13 22:49:45 +0800

    spring test

---
+++ src/test/java/com/jiurui/purchase/dao/BaseTestClass.java
@@ -0,0 +1,14 @@
+package com.jiurui.purchase.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by mark on 15/8/5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/applicationContext.xml"})
+public class BaseTestClass {
+
+}
---
+++ src/test/java/com/jiurui/purchase/dao/impl/TokenDaoImplTest.java
@@ -0,0 +1,31 @@
+package com.jiurui.purchase.dao.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mark on 15/9/13.
+ */
+public class TokenDaoImplTest {
+
+    @Test
+    public void testPersistence() throws Exception {
+
+    }
+
+    @Test
+    public void testGetToken() throws Exception {
+
+    }
+
+    @Test
+    public void testClearToken() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateToken() throws Exception {
+
+    }
+}
