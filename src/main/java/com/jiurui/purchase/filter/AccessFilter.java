c071435806fba7807763af83779fe51f018dbd0e
txlsy <334224109@qq.com>
2015-09-13 21:50:19 +0800

    add header

--- src/main/java/com/jiurui/purchase/filter/AccessFilter.java
+++ src/main/java/com/jiurui/purchase/filter/AccessFilter.java
@@ -17,7 +17,7 @@ public class AccessFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse)servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, token");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         filterChain.doFilter(servletRequest, response);
     }
